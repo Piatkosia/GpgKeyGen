dba7533abd12b95f5455292ce69a0a0dee6ef424
Angelika Maria Piątkowska (piatkosia) <piatkosia.apt@interia.pl>
2019-01-25 22:12:08 +0100

    Init

---
+++ GpgKeyGen/GpgKeyGen.Ui.Tests/GeneratorParamsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GpgKeyGen.Ui.Tests
+{
+    public class GeneratorParamsTests
+    {
+        [Fact]
+        public void ParamsConverter_IfForCommission_Returns1asExpiredInDays()
+        {
+            GeneratorParams par = new GeneratorParams();
+            par.Comment = "usdvuyfsgdf";
+            par.Email = "iasgf@sd.pl";
+            par.Password = "uiadfusdif";
+            par.ForCommission = true;
+            par.Username = "asygfuysgfigreg";
+            var tosend = par.ToGpgKeygenParams();
+            Assert.True(tosend.ExpiredInDays == 1);
+        }
+        [Fact]
+        public void ParamsConverter_IfNotForCommission_Returns0asExpiredInDays()
+        {
+            GeneratorParams par = new GeneratorParams();
+            par.Comment = "usdvuyfsgdf";
+            par.Email = "iasgf@sd.pl";
+            par.Password = "uiadfusdif";
+            par.ForCommission = false;
+            par.Username = "asygfuysgfigreg";
+            var tosend = par.ToGpgKeygenParams();
+            Assert.True(tosend.ExpiredInDays == 0);
+        }
+    }
+}
---
+++ GpgKeyGen/GpgKeyGen/GeneratorParams.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GpgKeyGenWrapper;
+
+namespace GpgKeyGen
+{
+    public class GeneratorParams
+    {
+        public string Username { get; set; }
+        public string Comment { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool ForCommission { get; set; }
+
+        public GpgKeygenParams ToGpgKeygenParams()
+        {
+            return new GpgKeygenParams()
+            {
+                Comment = this.Comment,
+                Email = this.Email,
+                ExpiredInDays = ForCommission ? 1 : 0,
+                Password = this.Password,
+                Username = this.Username,
+            };
+        }
+    }
+}
