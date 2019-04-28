e54da42a8a5cabf0b3a24ce4502f2f527ef933c6
Angelika Maria Piątkowska (piatkosia) <piatkosia.apt@interia.pl>
2019-04-28 21:12:50 +0200

    Add name validator

---
+++ GpgKeyGen/GpgKeyGen/Validators/NameCorrect.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace GpgKeyGen.Validators
+{
+    class NameCorrect: ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            ValidationResult validationResult = new ValidationResult(false, "Dane nieprawidłowe");
+            if (value != null && IsName(value.ToString())) validationResult = ValidationResult.ValidResult;
+            return validationResult;
+        }
+        private bool IsName(string stringValue)
+        {
+            string nameregex =  "^[a-zA-Z0-9óąśłżźćńĘÓĄŚŁŻŹĆŃ ]+$";
+            Match match = Regex.Match(stringValue, nameregex);
+            return match.Success;
+
+        }
+    }
+}
