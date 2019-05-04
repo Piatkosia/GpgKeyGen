d5f8aaff4eabe4176b4334ec4abf67190cd15dcd
Angelika Maria Piątkowska (piatkosia) <piatkosia.apt@interia.pl>
2019-05-04 20:50:33 +0200

    Set icon

--- GpgKeyGen/GpgKeyGen/Validators/HexNumberCorrect.cs
+++ GpgKeyGen/GpgKeyGen/Validators/HexNumberCorrect.cs
@@ -1,20 +1,25 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 
 namespace GpgKeyGen.Validators
 {
-    class NameCorrect: ValidationRule
+    public class HexNumberCorrect : ValidationRule
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
             ValidationResult validationResult = new ValidationResult(false, "Dane nieprawidłowe");
-            if (value != null && IsName(value.ToString())) validationResult = ValidationResult.ValidResult;
+            if (value != null && IsHexNumber(value.ToString())) validationResult = ValidationResult.ValidResult;
             return validationResult;
         }
-        private bool IsName(string stringValue)
+        private bool IsHexNumber(string stringValue)
         {
-            string nameregex =  "^[a-zA-Z0-9óąśłżźćńĘÓĄŚŁŻŹĆŃ ]+$";
+            string nameregex = "^[a-fA-F0-9]+$";
             Match match = Regex.Match(stringValue, nameregex);
             return match.Success;
         }
